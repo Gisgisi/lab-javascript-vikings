2b15aa1ca85cd03f855532f41eaa4f550d21a2ec
John Doe <atarod.shirin@gmail.com>
2022-10-07 14:12:57 +0200

    iteration2 done

--- src/viking.js
+++ src/viking.js
@@ -1,22 +1,38 @@
 // Soldier
 class Soldier {
-    constructor(health, strength){
+    constructor(health, strength) {
         this.health = health;
         this.strength = strength;
     }
-    attack(){
+    attack() {
         return this.strength;
     }
-    receiveDamage(damage){
+    receiveDamage(damage) {
         this.health -= damage;
     }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return this.name + " has received " + damage + " points of damage";
+        }
+        else { return this.name + " has died in act of combat"; }
+    }
+    battleCry(){
+        return `Odin Owns You All!`
+    }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
