ecbd2f704b4fdc3f57a9edead215f8af81c09b59
John Doe <atarod.shirin@gmail.com>
2022-10-07 17:14:59 +0200

    iteration5 done

--- src/viking.js
+++ src/viking.js
@@ -77,5 +77,16 @@ class War {
         }
         return vikingD
     }
-    showStatus() { }
+
+    showStatus() {
+        if(this.saxonArmy.length === 0){
+            return "Vikings have won the war of the century!"
+        }
+        else if(this.vikingArmy.length === 0 ){
+            return "Saxons have fought for their lives and survived another day..."
+        }
+        else if(this.saxonArmy.length>=1 && this.vikingArmy.length>=1){
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+     }
 }
